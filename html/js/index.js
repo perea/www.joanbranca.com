140bdc5815eecf0d84d95540b4c231d8d92596f5
Rick Bradley <rick@rickbradley.com>
2010-05-26 23:29:33 -0500

    index page has galleries as 'tabs'

--- html/js/index.js
+++ html/js/index.js
@@ -1,4 +1,10 @@
 $(function() {
   $('a.lightbox').lightBox(); // Select all links with lightbox class
+
+  $('a.nav_services').click(
+    function() {
+      $('.gallerytab').hide();
+      $(".gallerytab[rel='" + this.rel + "']").show();
+    })
 });
 
