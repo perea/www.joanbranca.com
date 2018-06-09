90a5556c38dd3d14bbc5c0da285eacb2fa97cf75
adam m pere <adampere001@gmail.com>
2018-06-09 13:08:08 -0400

    more updates and a bit of responsiveness

--- html/js/portfolios.js
+++ html/js/portfolios.js
@@ -383,12 +383,108 @@ var allImages = {
 			"url": "portfolio.html#maine",
 			"images": [
 				{
+					"fullURL": "imgs/maine/full/maine1_Footnote9x12.oiljpg.png",
+					"smallURL": "imgs/maine/150_sq/maine1_Footnote9x12.oiljpg.png",
+					"title": "Footnote",
+					"medium": "Oil",
+					"size": "9x12",
+					"index": "#3:1"
+                },
+				{
+					"fullURL": "imgs/maine/full/maine2_LittleCranberry6x8oil.png",
+					"smallURL": "imgs/maine/150_sq/maine2_LittleCranberry6x8oil.png",
+					"title": "Little Cranberry",
+					"medium": "Oil",
+					"size": "6x8",
+					"index": "#3:2"
+                },
+				{
+					"fullURL": "imgs/maine/full/maine3_Emma Marie 8x10oil.png",
+					"smallURL": "imgs/maine/150_sq/maine3_Emma Marie 8x10oil.png",
+					"title": "Emma Marie",
+					"medium": "Oil",
+					"size": "8x10",
+					"index": "#3:3"
+                },
+				{
+					"fullURL": "imgs/maine/full/maine4_Windsong_8x10oil.png",
+					"smallURL": "imgs/maine/150_sq/maine4_Windsong_8x10oil.png",
+					"title": "Windsong",
+					"medium": "Oil",
+					"size": "8x10",
+					"index": "#3:4"
+                },
+				{
+					"fullURL": "imgs/maine/full/maine5_Crow Island Fog8x10.oil.png",
+					"smallURL": "imgs/maine/150_sq/maine5_Crow Island Fog8x10.oil.png",
+					"title": "Crow Island Fog",
+					"medium": "Oil",
+					"size": "8x10",
+					"index": "#3:5"
+                },
+				{
+					"fullURL": "imgs/maine/full/maine6_Maine Island Marsh.oil.8x10.png",
+					"smallURL": "imgs/maine/150_sq/maine6_Maine Island Marsh.oil.8x10.png",
+					"title": "Maine Island Marsh",
+					"medium": "Oil",
+					"size": "8x10",
+					"index": "#3:6"
+                },
+				{
+					"fullURL": "imgs/maine/full/maine7_Sailboat in fog._8x10oil.8x10jpg.png",
+					"smallURL": "imgs/maine/150_sq/maine7_Sailboat in fog._8x10oil.8x10jpg.png",
+					"title": "Sailboat in Fog",
+					"medium": "Oil",
+					"size": "8x10",
+					"index": "#3:7"
+                },
+				{
+					"fullURL": "imgs/maine/full/maine8_At the Float8x10jpg.png",
+					"smallURL": "imgs/maine/150_sq/maine8_At the Float8x10jpg.png",
+					"title": "At the Float",
+					"medium": "Oil",
+					"size": "8x10",
+					"index": "#3:8"
+                },
+				{
+					"fullURL": "imgs/maine/full/maine9_Boat housesMorning light6x8oil.png",
+					"smallURL": "imgs/maine/150_sq/maine9_Boat housesMorning light6x8oil.png",
+					"title": "Boat Houses Morning Light",
+					"medium": "Oil",
+					"size": "6x8",
+					"index": "#3:9"
+                },
+				{
+					"fullURL": "imgs/maine/full/maine10_Lupine and barn_9x12_oil.png",
+					"smallURL": "imgs/maine/150_sq/maine10_Lupine and barn_9x12_oil.png",
+					"title": "Lupine and Barn",
+					"medium": "Oil",
+					"size": "9x12",
+					"index": "#3:10"
+                },
+				{
+					"fullURL": "imgs/maine/full/maine11_Rocks and Sunlight_8x10.png",
+					"smallURL": "imgs/maine/150_sq/maine11_Rocks and Sunlight_8x10.png",
+					"title": "Rocks and Sunlight",
+					"medium": "Oil",
+					"size": "8x10",
+					"index": "#3:11"
+                },
+				{
+					"fullURL": "imgs/maine/full/maine12_BlackLedges8x10 oil).png",
+					"smallURL": "imgs/maine/150_sq/maine12_BlackLedges8x10 oil).png",
+					"title": "Black Ledges",
+					"medium": "Oil",
+					"size": "8x10",
+					"index": "#3:12"
+                },
+				{
 					"fullURL": "imgs/maine/full/1-1.fishing boat.9x12.oil.png",
 					"smallURL": "imgs/maine/150_sq/1-1.fishing boat.9x12.oil.png",
 					"title": "Fishing Boat",
 					"medium": "Oil",
 					"size": "9x18",
-					"index": "#3:1"
+					"index": "#3:13"
                 },
 				{
 					"fullURL": "imgs/maine/full/1-2.maine fishing.9x12.oil.png",
@@ -396,7 +492,7 @@ var allImages = {
 					"title": "Maine Fishing",
 					"medium": "Oil",
 					"size": "9x12",
-					"index": "#3:2"
+					"index": "#3:14"
                 },
 				{
 					"fullURL": "imgs/maine/full/1-3.Path to Bunkers Cove.mixed media.9x12.png",
@@ -404,7 +500,7 @@ var allImages = {
 					"title": "Path to Bunkers Cove",
 					"medium": "Mixed Media",
 					"size": "9x12",
-					"index": "#3:3"
+					"index": "#3:15"
                 },
 				{
 					"fullURL": "imgs/maine/full/1-4.Island_Fog.8x10.oil.png",
@@ -412,7 +508,7 @@ var allImages = {
 					"title": "Island Fog",
 					"medium": "Oil",
 					"size": "8x10",
-					"index": "#3:4"
+					"index": "#3:16"
                 },
 				{
 					"fullURL": "imgs/maine/full/2-1.Morning Fog.8x10.oil.png",
@@ -420,7 +516,7 @@ var allImages = {
 					"title": "Morning Fog",
 					"medium": "Oil",
 					"size": "6x18",
-					"index": "#3:5"
+					"index": "#3:17"
                 },
 				{
 					"fullURL": "imgs/maine/full/2-2.Hadlock Point.mixed media.9x12.png",
@@ -428,7 +524,7 @@ var allImages = {
 					"title": "Hadlock Point",
 					"medium": "Mixed Media",
 					"size": "9x12",
-					"index": "#3:6"
+					"index": "#3:18"
                 },
 				{
 					"fullURL": "imgs/maine/full/2-3.Towards  Gilley Beach.8x12.mixedmedia.png",
@@ -436,7 +532,7 @@ var allImages = {
 					"title": "Towards Gilley Beach",
 					"medium": "Mixed Media",
 					"size": "8x12",
-					"index": "#3:7"
+					"index": "#3:19"
                 },
 				{
 					"fullURL": "imgs/maine/full/2-4.Marsh Trees.mixed media.9x12.png",
@@ -444,7 +540,7 @@ var allImages = {
 					"title": "Marsh Trees",
 					"medium": "Mixed Media",
 					"size": "9x12",
-					"index": "#3:8"
+					"index": "#3:20"
                 },
 				{
 					"fullURL": "imgs/maine/full/3-1.Marsh Pool.8x10.oil.png",
@@ -452,7 +548,7 @@ var allImages = {
 					"title": "Marsh Pool",
 					"medium": "Oil",
 					"size": "8x10",
-					"index": "#3:9"
+					"index": "#3:21"
                 },
 				{
 					"fullURL": "imgs/maine/full/3-2.Lupine.9x12.png",
@@ -460,7 +556,7 @@ var allImages = {
 					"title": "Lupine",
 					"medium": "Oil",
 					"size": "9x12",
-					"index": "#3:10"
+					"index": "#3:22"
                 },
 				{
 					"fullURL": "imgs/maine/full/3-3.FromEvelynshouse.9x12.oil.png",
@@ -468,7 +564,7 @@ var allImages = {
 					"title": "From Evelyn's House",
 					"medium": "Oil",
 					"size": "9x12",
-					"index": "#3:11"
+					"index": "#3:23"
                 }
             ]
         },
@@ -477,52 +573,28 @@ var allImages = {
 			"url": "portfolio.html#voyages",
 			"images": [
 				{
-					"fullURL": "imgs/voyages/full/1-2.May I cook this for you_.9x12.png",
-					"smallURL": "imgs/voyages/150_sq/1-2.May I cook this for you_.9x12.png",
-					"title": "May I Cook This For You?",
+					"fullURL": "imgs/voyages/full/voyage1_Veneration _84x60_oil.png",
+					"smallURL": "imgs/voyages/150_sq/voyage1_Veneration _84x60_oil.png",
+					"title": "Veneration",
 					"medium": "Oil",
-					"size": "9x12",
+					"size": "84x60",
 					"index": "#4:1"
                 },
 				{
-					"fullURL": "imgs/voyages/full/1-3.Fish Dream 16x20.oil.png",
-					"smallURL": "imgs/voyages/150_sq/1-3.Fish Dream 16x20.oil.png",
-					"title": "Fish Dream",
-					"medium": "Oil",
-					"size": "16x20",
+					"fullURL": "imgs/voyages/full/voyage2_El senor de Esquipulas 80x80pastel.png",
+					"smallURL": "imgs/voyages/150_sq/voyage2_El senor de Esquipulas 80x80pastel.png",
+					"title": "El senor de Esquipulas",
+					"medium": "Pastel",
+					"size": "80x80",
 					"index": "#4:2"
                 },
 				{
-					"fullURL": "imgs/voyages/full/1-4.If she dreams of fish_18x22_oil.png",
-					"smallURL": "imgs/voyages/150_sq/1-4.If she dreams of fish_18x22_oil.png",
-					"title": "If She Dreams of Fish",
-					"medium": "Oil",
-					"size": "18x22",
+					"fullURL": "imgs/voyages/full/voyage3_Dancing Dream_ 30x22mixed media.png",
+					"smallURL": "imgs/voyages/150_sq/voyage3_Dancing Dream_ 30x22mixed media.png",
+					"title": "Dancing Dream",
+					"medium": "Mixed Media",
+					"size": "30x22",
 					"index": "#4:3"
-                },
-				{
-					"fullURL": "imgs/voyages/full/2-1.Moonlit Conversation_18x22_oil.png",
-					"smallURL": "imgs/voyages/150_sq/2-1.Moonlit Conversation_18x22_oil.png",
-					"title": "Moonlit Conversation",
-					"medium": "Oil",
-					"size": "18x22",
-					"index": "#4:4"
-                },
-				{
-					"fullURL": "imgs/voyages/full/2-2.Towards Eden_18x22_oil.png",
-					"smallURL": "imgs/voyages/150_sq/2-2.Towards Eden_18x22_oil.png",
-					"title": "Towards Eden",
-					"medium": "Oil",
-					"size": "18x22",
-					"index": "#4:5"
-                },
-				{
-					"fullURL": "imgs/voyages/full/2-3.Wetlands and Apple 16x20.oil.png",
-					"smallURL": "imgs/voyages/150_sq/2-3.Wetlands and Apple 16x20.oil.png",
-					"title": "Wetlands and Apple",
-					"medium": "Oil",
-					"size": "16x20",
-					"index": "#4:6"
                 },
 				{
 					"fullURL": "imgs/voyages/full/2-4.animal head with figures.30x42.oil.png",
@@ -530,7 +602,56 @@ var allImages = {
 					"title": "Animal Head with Figures",
 					"medium": "Oil",
 					"size": "30x42",
+					"index": "#4:4"
+                },
+				
+				{
+					"fullURL": "imgs/voyages/full/1-2.May I cook this for you_.9x12.png",
+					"smallURL": "imgs/voyages/150_sq/1-2.May I cook this for you_.9x12.png",
+					"title": "May I Cook This For You?",
+					"medium": "Oil",
+					"size": "9x12",
+					"index": "#4:5"
+                },
+				{
+					"fullURL": "imgs/voyages/full/1-3.Fish Dream 16x20.oil.png",
+					"smallURL": "imgs/voyages/150_sq/1-3.Fish Dream 16x20.oil.png",
+					"title": "Fish Dream",
+					"medium": "Oil",
+					"size": "16x20",
+					"index": "#4:6"
+                },
+				{
+					"fullURL": "imgs/voyages/full/1-4.If she dreams of fish_18x22_oil.png",
+					"smallURL": "imgs/voyages/150_sq/1-4.If she dreams of fish_18x22_oil.png",
+					"title": "If She Dreams of Fish",
+					"medium": "Oil",
+					"size": "18x22",
 					"index": "#4:7"
+                },
+				{
+					"fullURL": "imgs/voyages/full/2-1.Moonlit Conversation_18x22_oil.png",
+					"smallURL": "imgs/voyages/150_sq/2-1.Moonlit Conversation_18x22_oil.png",
+					"title": "Moonlit Conversation",
+					"medium": "Oil",
+					"size": "18x22",
+					"index": "#4:8"
+                },
+				{
+					"fullURL": "imgs/voyages/full/2-2.Towards Eden_18x22_oil.png",
+					"smallURL": "imgs/voyages/150_sq/2-2.Towards Eden_18x22_oil.png",
+					"title": "Towards Eden",
+					"medium": "Oil",
+					"size": "18x22",
+					"index": "#4:9"
+                },
+				{
+					"fullURL": "imgs/voyages/full/2-3.Wetlands and Apple 16x20.oil.png",
+					"smallURL": "imgs/voyages/150_sq/2-3.Wetlands and Apple 16x20.oil.png",
+					"title": "Wetlands and Apple",
+					"medium": "Oil",
+					"size": "16x20",
+					"index": "#4:10"
                 },
 				{
 					"fullURL": "imgs/voyages/full/3-1.Jump.16x42.mixed media collage.png",
@@ -538,7 +659,7 @@ var allImages = {
 					"title": "Jump",
 					"medium": "Mixed Media",
 					"size": "16x42",
-					"index": "#4:8"
+					"index": "#4:11"
                 },
 				{
 					"fullURL": "imgs/voyages/full/3-2.Save.10x10.mixed media.png",
@@ -546,7 +667,7 @@ var allImages = {
 					"title": "Save",
 					"medium": "Mixed Media",
 					"size": "10x10",
-					"index": "#4:9"
+					"index": "#4:12"
                 },
 				{
 					"fullURL": "imgs/voyages/full/3-3.Dolphin Dream.12x11.5 mixed media collage.png",
@@ -554,7 +675,7 @@ var allImages = {
 					"title": "Dolphin Dream",
 					"medium": "Mixed Media Collage",
 					"size": "12x11.5",
-					"index": "#4:10"
+					"index": "#4:13"
                 },
 				{
 					"fullURL": "imgs/voyages/full/3-4.The Memory Palace .40x42.png",
@@ -562,7 +683,7 @@ var allImages = {
 					"title": "The Memory Palace",
 					"medium": "Mixed Media",
 					"size": "40x42",
-					"index": "#4:11"
+					"index": "#4:14"
                 }
             ]
         },
@@ -571,12 +692,76 @@ var allImages = {
 			"url": "portfolio.html#land",
 			"images": [
 				{
+					"fullURL": "imgs/land/full/land1_Granvilles garden_16x20_oil 2016.png",
+					"smallURL": "imgs/land/150_sq/land1_Granvilles garden_16x20_oil 2016.png",
+					"title": "Granville's Garden",
+					"medium": "Oil",
+					"size": "16x20",
+					"index": "#5:1"
+                },
+				{
+					"fullURL": "imgs/land/full/land2_Lilies and Hydrangea16x20oil.png",
+					"smallURL": "imgs/land/150_sq/land2_Lilies and Hydrangea16x20oil.png",
+					"title": "Lilies and Hydrangea",
+					"medium": "Oil",
+					"size": "16x20",
+					"index": "#5:2"
+                },
+				{
+					"fullURL": "imgs/land/full/land3_Peony Buds and Clematis.oil.30x22.png",
+					"smallURL": "imgs/land/150_sq/land3_Peony Buds and Clematis.oil.30x22.png",
+					"title": "Peony Buds and Clematis",
+					"medium": "Oil",
+					"size": "30x22",
+					"index": "#5:3"
+                },
+				{
+					"fullURL": "imgs/land/full/land4_Wild Garden of the heart 9x14.5_oil.png",
+					"smallURL": "imgs/land/150_sq/land4_Wild Garden of the heart 9x14.5_oil.png",
+					"title": "Wild Garden of the Heart",
+					"medium": "Oil",
+					"size": "9x14",
+					"index": "#5:4"
+                },
+				{
+					"fullURL": "imgs/land/full/land5_Early winter afternoon.20x16.oil.png",
+					"smallURL": "imgs/land/150_sq/land5_Early winter afternoon.20x16.oil.png",
+					"title": "Early Winter Afternoon",
+					"medium": "Oil",
+					"size": "20x16",
+					"index": "#5:5"
+                },
+				{
+					"fullURL": "imgs/land/full/Land6_Phragmites in Winter 9x12_oil.png",
+					"smallURL": "imgs/land/150_sq/Land6_Phragmites in Winter 9x12_oil.png",
+					"title": "Phragmites in Winter",
+					"medium": "Oil",
+					"size": "9x12",
+					"index": "#5:6"
+                },
+				{
+					"fullURL": "imgs/land/full/land7_Spring farming.8x10oil.png",
+					"smallURL": "imgs/land/150_sq/land7_Spring farming.8x10oil.png",
+					"title": "Spring Farming",
+					"medium": "Oil",
+					"size": "8x10",
+					"index": "#5:7"
+                },
+				{
+					"fullURL": "imgs/land/full/land8_Rose of Sharon dream_8x10oil.png",
+					"smallURL": "imgs/land/150_sq/land8_Rose of Sharon dream_8x10oil.png",
+					"title": "Rose of Sharon Dream",
+					"medium": "Oil",
+					"size": "8x10",
+					"index": "#5:8"
+                },
+				{
 					"fullURL": "imgs/land/full/1-1.Peonies in Full Sun.9x12.oil.png",
 					"smallURL": "imgs/land/150_sq/1-1.Peonies in Full Sun.9x12.oil.png",
 					"title": "Peonies in Full Sun",
 					"medium": "Oil",
 					"size": "9x12",
-					"index": "#5:1"
+					"index": "#5:9"
                 },
 				{
 					"fullURL": "imgs/land/full/wild_rose_8x10.jpg",
@@ -584,7 +769,7 @@ var allImages = {
 					"title": "Wild Roses",
 					"medium": "Oil",
 					"size": "8x10",
-					"index": "#5:2"
+					"index": "#5:10"
                 },
 				{
 					"fullURL": "imgs/land/full/Light_in_the_Garden.28 x22.oil.jpg",
@@ -592,7 +777,7 @@ var allImages = {
 					"title": "Light in the Garden",
 					"medium": "Oil",
 					"size": "28x22",
-					"index": "#5:3"
+					"index": "#5:11"
                 },
 				{
 					"fullURL": "imgs/land/full/1-4.Reaching_for_the_Sun.28x22.oil.png",
@@ -600,7 +785,7 @@ var allImages = {
 					"title": "Reaching for the Sun",
 					"medium": "Oil",
 					"size": "28x22",
-					"index": "#5:4"
+					"index": "#5:12"
                 },
 				{
 					"fullURL": "imgs/land/full/morning_stroll_8x10.jpg",
@@ -608,7 +793,7 @@ var allImages = {
 					"title": "Morning Stroll",
 					"medium": "Oil",
 					"size": "8x10",
-					"index": "#5:5"
+					"index": "#5:13"
                 },
 				{
 					"fullURL": "imgs/land/full/walk_in_the_fog_8x10.jpg",
@@ -616,7 +801,7 @@ var allImages = {
 					"title": "Walk in the Fog",
 					"medium": "Oil",
 					"size": "8x10",
-					"index": "#5:6"
+					"index": "#5:14"
                 },
 
 				{
@@ -625,7 +810,7 @@ var allImages = {
 					"title": "Farm in July",
 					"medium": "Oil",
 					"size": "28x22",
-					"index": "#5:7"
+					"index": "#5:15"
                 },
 				{
 					"fullURL": "imgs/land/full/2-3.Vinyard Number 5.20x16.oil.png",
@@ -633,7 +818,7 @@ var allImages = {
 					"title": "Vineyard Number 5",
 					"medium": "Oil",
 					"size": "20x16",
-					"index": "#5:8"
+					"index": "#5:16"
                 }
             ]
         }
